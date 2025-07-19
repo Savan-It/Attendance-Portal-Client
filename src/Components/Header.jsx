45f2bbaf7541d36d85c03036aa9b99d97cfe04c1
Savan Prajapati <savanprajapati.it@gmail.com>
2025-07-20 02:36:52 +0530

    [ADD] update site name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,14 +14,14 @@ function Header({ setIsLogin }) {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Attendance</Link>
+          <Link className="navbar-brand" to="/">SiteStaff</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/salary">Salary</Link>
+                <Link className="nav-link" to="/">Attendance</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/upad">Upad</Link>
